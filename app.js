51be51bc5a78c6b55d1d9801b5c387db291ba2af
Shivam Singh <shivamsinghps@gmail.com>
2021-12-02 01:58:24 +0530

    combined and server side rendering completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const mongoose = require("./src/Middlewares/database");
 const apis = require("./src/Endpoints/routes");
@@ -10,17 +11,22 @@ const apis = require("./src/Endpoints/routes");
 const app = express();
 mongoose.dbConnect();
 app.use(morgan("dev"));
-app.use(helmet());
+// app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static("public"));
+
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
 app.use("/api", apis);
 
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
 
 module.exports = app;
-
-// "node-cron": "^3.0.0",
-// "stocksocket": "^2.1.2",
-// "yahoo-finance": "^0.3.7",
-// "yfinance-live": "^0.3.3"
--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 9001;
 
-app.listen(PORT, () => console.log(`Server starting on port ${PORT}!`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server starting on port ${PORT}!`));
